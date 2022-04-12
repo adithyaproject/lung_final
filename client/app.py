a9cccf5755e5f4e7af2e4ddba8592aa093084718
adithyaproject <adithyaproject01@gmail.com>
2022-04-13 01:22:35 +0530

    added OLED module library Adafruit and updated client app file by adding comments related to that

--- client/app.py
+++ client/app.py
@@ -1,3 +1,7 @@
+# Uncomment below 3 lines if you are dealing with Raspberry pi & OLED display
+# import sys
+# sys.path.insert(0,"/home/Your_project_directory/Adafruit_Python_SSD1306/examples")
+# import text
 import recording
 import mfcc
 import requests
@@ -15,3 +19,6 @@ if(recording.record_sound()):
     # Send post request for tha backend by passing encoded list
     res = requests.post("http://192.168.8.101:5000/", json = {"data":serializedMfcc})
     print(res.json()["condition"])
+
+    # Uncomment below line if you are dealing with Raspberry pi & OLED display
+    # text.displayText(res.json()["condition"])
